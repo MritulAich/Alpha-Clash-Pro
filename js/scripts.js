bd2ec17d4c97ca42f1a330d217cc37cc4a7b42f8
MritulAich <mridulaich332@gmail.com>
2024-02-16 17:55:45 +0600

    created the game

--- js/scripts.js
+++ js/scripts.js
@@ -11,19 +11,98 @@
 
 function continueGame() {
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet', alphabet);
+    // console.log('your random alphabet', alphabet);
 
     //set it to screen show
     const currentAlphabetElement = document.getElementById('current-alphabet');
     currentAlphabetElement.innerText = alphabet;
+
+    //set bg color
+    setBgColorById(alphabet);
 }
 
 //..alternative of hide & show..//
 function play() {
     hideElementById('home-screen');
+    hideElementById('score');
     showElementById('play-ground');
+
+    //reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
+
     continueGame();
 }
 
 
 
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('score');
+
+
+    ///get final score
+    const finalScore = getTextElementValueById('current-score');
+    console.log(finalScore);
+    setTextElementValueById('final-score', finalScore);
+
+
+    //clear previous alphabet
+    const currentAlphabet = getElementTextById('current-alphabet');
+    removeBgColor(currentAlphabet);
+}
+
+
+
+function handleKeyboardKeyUpEvent(event) {
+    const playerPressed = event.key;
+    console.log('player pressed', playerPressed);
+
+    //stop game if pressed 'Esc'
+    if(playerPressed === 'Escape'){
+        gameOver();
+    } 
+
+
+    //get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    console.log(playerPressed, currentAlphabet);
+
+
+    //check matched or not
+    if (playerPressed === expectedAlphabet) {
+        console.log('you got a point');
+
+      //update score: 
+        const currentScoreText = document.getElementById('current-score');
+        const currentScore = parseInt(currentScoreText.innerText);
+
+        const newScore = currentScore + 1;
+
+        currentScoreText.innerText = newScore;
+
+
+        //start new round
+        removeBgColor(expectedAlphabet);
+        continueGame();
+    }
+    else {
+        console.log('you missed');
+
+        //get current life number
+        const currentLifeText = document.getElementById('current-life');
+        const currentLife = parseInt(currentLifeText.innerText);
+        //reduce
+        const newLife = currentLife - 1;
+        //then display it
+        currentLifeText.innerText = newLife;
+
+        if(newLife === 0){
+            gameOver() ;
+        }
+    } 
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
--- js/utility.js
+++ js/utility.js
@@ -8,12 +8,47 @@ function showElementById(elementId) {
     element.classList.remove('hidden');
 }
 
+
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const value = parseInt(element.innerText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
 function getARandomAlphabet() {
     const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-    console.log(alphabets);
 
     const randomNumber = Math.round(Math.random() * 25);
    
     const alphabet = alphabets[randomNumber];
     return alphabet;
+}
+
+
+
+
+function setBgColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+
+
+function removeBgColor(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
 }
